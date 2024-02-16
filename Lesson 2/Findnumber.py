1eb0f28c1166e374110d46c325a0ec4b0ae60c1f
Юри1 <tomcat2393@gmail.com>
2024-02-16 18:30:06 +0300

    Реализуйте структуру данных «Участники спора». Она должна представлять собой список (list) словарей (dict). Каждый словарь хранит информацию об отдельном участнике. Словарь содержит пары «ключ-значение» параметрами, то есть характеристиками участника: наименование, статус, ИНН. Заполнение данных должно быть произведено пользователем через консоль для трёх различных участников. Пример готовой структуры: [ {“name”: 'ООО "Рога и Копыта"', “status”: "Истец", “inn”: "4545454545"}, {“name”: 'Баширов А.А.', “status”: "Ответчик", “inn”: "32323232323232"}, {“name”: 'Петров А.А.', “status”: "Третье лицо", “inn”: "12121212121212"}

---
+++ Lesson 2/Findnumber.py
@@ -0,0 +1,11 @@
+def find_max_of_three_numbers():
+    numbers = [int(input("Введите первое число: ")),
+               int(input("Введите второе число: ")),
+               int(input("Введите третье число: "))]
+
+    max_number = max(numbers)
+
+    print("Наибольшее число:", max_number)
+
+
+find_max_of_three_numbers()
